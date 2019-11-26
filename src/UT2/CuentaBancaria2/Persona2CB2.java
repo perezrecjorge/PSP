2997dfd51ecdd5b92178cf5495713210a5429444
perezrecjorge <perezrecjorge@gmail.com>
2019-11-26 23:54:31 +0100

    Subir ejemplo UT3

--- src/UT2/CuentaBancaria2/Persona2CB2.java
+++ src/UT2/CuentaBancaria2/Persona2CB2.java
@@ -4,7 +4,6 @@ public class Persona2CB2 extends Thread {
     CuentaCB2 cuenta;
     String nombre;
     int cantidad;
-    int n;
 
     public Persona2CB2(String nombre, CuentaCB2 cuenta) {
         this.nombre = nombre.toUpperCase();
@@ -14,7 +13,7 @@ public class Persona2CB2 extends Thread {
     @Override
     public void run() {
         try {
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 2; i++) {
                 cuenta.reintegro((int) (Math.random() * 500 + 1), this.nombre);
                 sleep(1000);
             }
